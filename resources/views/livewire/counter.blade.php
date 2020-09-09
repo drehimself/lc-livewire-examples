f482db345039907b258eeffa1f33404c4e5a3d56
Andre Madarang <andre_madarang@hotmail.com>
2020-09-09 03:07:49 -0400

    Episode 1 - Intro

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <span>{{ $count }}</span>
+    <button wire:click="decrement">-</button>
+    <button wire:click="increment">+</button>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <livewire:styles />
+
+        <style>
+            body {
+                font-family: 'Nunito';
+            }
+        </style>
+    </head>
+    <body>
+        <livewire:counter />
+        <livewire:scripts />
+    </body>
+</html>
